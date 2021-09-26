7bb01d1313bff2541bf69a72f0759011b7aa44c6
Ben Roush <benjamin.c.roush@gmail.com>
2021-09-26 17:13:00 -0400

    Adding Basic Auth to Swagger docs

--- NoInc.TestProject/Startup.cs
+++ NoInc.TestProject/Startup.cs
@@ -37,6 +37,29 @@ namespace NoInc.TestProject
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "NoInc.TestProject", Version = "v1" });
+                c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "basic",
+                    In = ParameterLocation.Header,
+                    Description = "Basic Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "basic"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
             });
             services.AddMvc();
             services.AddCors(options =>
